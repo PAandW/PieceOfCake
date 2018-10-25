afd8540629e8dd8020394663c6f21707d6d25f97
Peter Wells <pwells@myriadmobile.com>
2018-10-24 19:30:48 -0500

    Created converter factory to handle ingredient JSONP response

--- app/src/main/java/com/paandw/pieceofcake/data/domain/IngredientDomain.java
+++ app/src/main/java/com/paandw/pieceofcake/data/domain/IngredientDomain.java
@@ -1,6 +1,8 @@
 package com.paandw.pieceofcake.data.domain;
 
+import com.google.gson.Gson;
 import com.paandw.pieceofcake.data.AppService;
+import com.paandw.pieceofcake.data.util.IngredientConverterFactory;
 
 import retrofit2.Retrofit;
 
@@ -11,6 +13,7 @@ public class IngredientDomain extends BaseDomain {
     public IngredientDomain() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://api.yummly.com/v1/api")
+                .addConverterFactory(new IngredientConverterFactory(new Gson()))
                 .build();
 
         appService = retrofit.create(AppService.class);
---
+++ app/src/main/java/com/paandw/pieceofcake/data/util/IngredientResponseBodyConverter.java
@@ -0,0 +1,39 @@
+package com.paandw.pieceofcake.data.util;
+
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+
+import java.io.IOException;
+import java.io.Reader;
+
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+
+public class IngredientResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+
+    private final Gson gson;
+    private final TypeAdapter<T> adapter;
+
+    public IngredientResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+        this.gson = gson;
+        this.adapter = adapter;
+    }
+
+    @Override
+    public T convert(ResponseBody value) throws IOException {
+        Reader reader = value.charStream();
+        int item = reader.read();
+        while (item != '(' && item != -1) {
+            item = reader.read();
+        }
+
+        JsonReader jsonReader = gson.newJsonReader(reader);
+        try {
+            return adapter.read(jsonReader);
+        } finally {
+            reader.close();
+        }
+    }
+}
